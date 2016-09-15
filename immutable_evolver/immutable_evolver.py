c6692c357ca4f857e712eeae3efe9c612ee699a5
Paul <paul@ruthorn.co.uk>
2016-09-15 22:57:13 +0100

    Initial commit of immutable evolver code

---
+++ immutable_evolver/immutable_evolver.py
@@ -0,0 +1,45 @@
+from abc import ABCMeta
+import inspect
+from collections import OrderedDict
+
+from pyrsistent import pmap
+
+
+def all_slots(cls):
+    slot_names = OrderedDict()
+    for base in inspect.getmro(cls):
+        slot_names.update(
+            (k, None) for k in getattr(base, '__slots__', ()))
+    return tuple(slot_names)
+
+
+class ImmutableEvolverMeta(ABCMeta):
+    def __new__(metacls, name, bases, cls_dict):
+        cls = super().__new__(metacls, name, bases, cls_dict)
+        cls._all_slots = all_slots(cls)
+        for base in bases:
+            all_defaults = getattr(base, '_all_defaults', None)
+            if all_defaults is not None:
+                cls._all_defaults = all_defaults.update(dict(zip(
+                    reversed(cls.__slots__), reversed(cls._defaults))))
+                break
+        return cls
+
+
+class ImmutableEvolver(metaclass=ImmutableEvolverMeta):
+    __slots__ = ()
+    _defaults = ()
+    _all_defaults = pmap()
+
+    def __init__(self, *args, **kwargs):
+        named_args = dict(zip(self._all_slots, args))
+        kwargs = {'_' + k: v for k, v in kwargs.items()}
+        assert not set(named_args) & set(kwargs)
+        kwargs = self._all_defaults.update(named_args, kwargs)
+        assert set(kwargs) == set(self._all_slots)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def _evolve(self, **kwargs):
+        members = ((k.lstrip('_'), getattr(self, k)) for k in self._all_slots)
+        return type(self)(**dict(members, **kwargs))
---
+++ immutable_evolver/test_immutable_evolver.py
@@ -0,0 +1,62 @@
+from pytest import raises
+
+from pyrsistent import pmap
+
+from .immutable_evolver import ImmutableEvolver
+
+
+class Foo(ImmutableEvolver):
+    __slots__ = '_a', '_b'
+    _defaults = 2,
+
+
+class Bar(Foo):
+    __slots__ = '_c', '_d'
+    _defaults = 4,
+
+
+def test_slots_accumulate():
+    assert Foo._all_slots == ('_a', '_b')
+    assert Bar._all_slots == ('_c', '_d', '_a', '_b')
+
+
+def test_defaults_correctly_attributed():
+    assert Foo._all_defaults == pmap({'_b': 2})
+    assert Bar._all_defaults == pmap({'_b': 2, '_d': 4})
+
+
+def test_arg_order():
+    bar = Bar(1, 2, 3, 4)
+    assert bar._a == 3
+    assert bar._b == 4
+    assert bar._c == 1
+    assert bar._d == 2
+
+
+def test_mandatory_kwargs():
+    bar = Bar(c=1, a=2)
+    assert bar._a == 2
+    assert bar._b == 2
+    assert bar._c == 1
+    assert bar._d == 4
+
+
+def test_args_kwargs():
+    bar = Bar(1, a=2)
+    assert bar._a == 2
+    assert bar._b == 2
+    assert bar._c == 1
+    assert bar._d == 4
+
+    bar2 = bar._evolve(c=6)
+    assert bar2 is not bar
+    assert bar._c == 1
+    assert bar2._c == 6
+    assert bar2._a == bar._a
+    assert bar2._b == bar._b
+    assert bar2._d == bar._d
+
+
+def test_assertion():
+    raises(AssertionError, Bar)
+    raises(AssertionError, Bar, 1, c=2)
